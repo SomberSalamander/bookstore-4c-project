296fa2025b7b14e02cfa7bbd55e4addc3d64362e
Barbora Volšická <VolsickaBarbora@litv.sssvt.cz>
2026-01-05 09:04:47 +0100

    feat: created project by template

---
+++ src/BookstoreWebApplication.WebMvcApp/BookstoreWebApplication.WebMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookstoreWebApplication.WebMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
