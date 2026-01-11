79b7b0469fdb9798e3f270216b50851930ebedb3
bev <volsickabarbora@sssvt.cz>
2026-01-11 16:46:12 +0100

    feat: added BooksDbContext
    feat: added BooksDbContext

---
+++ src/BookstoreWebApplication.WebMvcApp/BookstoreWebApplication.WebMvcApp/Data/BooksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using BookstoreWebApplication.WebMvcApp.Entities;
+
+namespace BookstoreWebApplication.WebMvcApp.Data
+{
+    public class BooksDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=mysqlstudenti.litv.sssvt.cz;database=mrkacek_db1;user=mrkacekread;password=123456");
+        }
+    }
+}
