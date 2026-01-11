f9622d863ec9a1201a4d7648f5f802258f97b8fb
bev <volsickabarbora@sssvt.cz>
2026-01-11 15:36:52 +0100

    fix: put Book class in Entities namespace
    fix: put Book class in Entities namespace

--- src/BookstoreWebApplication.WebMvcApp/BookstoreWebApplication.WebMvcApp/Entities/Book.cs
+++ src/BookstoreWebApplication.WebMvcApp/BookstoreWebApplication.WebMvcApp/Entities/Book.cs
@@ -1,50 +1,53 @@
 ﻿using System;
 
-public class Book
+namespace BookstoreWebApplication.WebMvcApp.Entities
 {
     public class Book
     {
-        // PK
-        public int BookID { get; set; }
-        
-        public string Title { get; set; }
+        public class Book
+        {
+            // PK
+            public int BookID { get; set; }
 
-        public int Author { get; set; }
+            public string Title { get; set; }
 
-        public int Publisher { get; set; }
+            public int Author { get; set; }
 
-        public string Description { get; set; }
+            public int Publisher { get; set; }
 
-        public float Price { get; set; }
+            public string Description { get; set; }
 
-        public int Stock { get; set; }
+            public float Price { get; set; }
 
-        public DateTime PublishedAt { get; set; }
+            public int Stock { get; set; }
 
-        // enum
-        public CoverType Cover { get; set; }
+            public DateTime PublishedAt { get; set; }
 
-        public DateTime Edition { get; set; }
+            // enum
+            public CoverType Cover { get; set; }
 
-        public string Language { get; set; }
+            public DateTime Edition { get; set; }
 
-        public string ISBN { get; set; }
+            public string Language { get; set; }
 
-        // examp. "20cm x 13cm x 2cm"
-        public string Dimensions { get; set; }
+            public string ISBN { get; set; }
 
-        // kg
-        public double Weight { get; set; }
+            // examp. "20cm x 13cm x 2cm"
+            public string Dimensions { get; set; }
 
-        public int NumberOfPages { get; set; }
+            // kg
+            public double Weight { get; set; }
 
-        // 0 - 5
-        public float Rating { get; set; }
+            public int NumberOfPages { get; set; }
 
-        // optional
-        public int? TranslatorID { get; set; }
-        public int? IllustratorID { get; set; }
+            // 0 - 5
+            public float Rating { get; set; }
+
+            // optional
+            public int? TranslatorID { get; set; }
+            public int? IllustratorID { get; set; }
+        }
+
+        public enum CoverType { Hard, Soft, Papercover }
     }
-
-    public enum CoverType { Hard, Soft, Papercover }
 }
